a1e6b3d43310d27db23d1ab4459074deb25a8be7
vasanthkumaran555-gif <vasanthkumaran555@gmail.com>
2026-08-18 11:56:36 +0530

    Time: 4 ms (13.86%) | Memory: 43.2 MB (12.45%) - LeetSync

---
+++ 388-longest-absolute-file-path/longest-absolute-file-path.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int lengthLongestPath(String input) {
+        int longest = 0;
+        Map<Integer, Integer> pathMap = new HashMap<>();
+        pathMap.put(0, 0);
+
+        String[] lines = input.split("\n");
+
+        for (String line : lines) {
+            String name = line.replaceAll("\t", "");
+            int depth = line.length() - name.length();
+
+            if (name.contains(".")) {
+                // It's a file
+                longest = Math.max(longest, pathMap.get(depth) + name.length());
+            } else {
+                // It's a directory
+                pathMap.put(depth + 1, pathMap.get(depth) + name.length() + 1);
+            }
+        }
+
+        return longest;
+    }
+}
